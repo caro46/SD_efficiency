1b5ef65d201c28ee66cade5fe2701eb336a07bd5
caroline <cauretcaroline@gmail.com>
2022-06-10 09:50:55 -0700

    fixing kallisto submission script

---
+++ kallisto_pseudocount_sub.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=star_mapping
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=1G
+#SBATCH --output=kallisto.%J.out
+#SBATCH --error=kallisto.%J.err
+#SBATCH --account=def-ben
+#SBATCH --mail-user=cauretc@oregonstate.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+module load kallisto/0.46.1
+
+#for indexing
+python3 ~/project/cauretc/scripts/KO_rnaseq/kallisto_pseudocount.py $@ --threads 1
+#for quantification
+#python3 ~/project/cauretc/scripts/KO_rnaseq/kallisto_pseudocount.py $@ --threads 6
+
+#sbatch ~/project/cauretc/scripts/KO_rnaseq/kallisto_pseudocount_sub.sh --ref_transcriptome /home/cauretc/projects/rrg-ben/cauretc/2021_KO_rnaseq/Xenla10_1_seq/XENLA_10.1_GCF.transcripts.fa.gz --step index
+#sbatch ~/project/cauretc/scripts/KO_rnaseq/kallisto_pseudocount_sub.sh --ref_transcriptome /home/cauretc/projects/rrg-ben/cauretc/2021_KO_rnaseq/Xenla10_1_seq/XENLA_10.1_GCF.transcripts.fa.gz --step quant --fastq /home/cauretc/projects/rrg-ben/ben/2021_XL_ko_tad_RNAseq/raw_data_3rd_run/scanw/*trim*.fastq.gz --output_directory /home/cauretc/projects/rrg-ben/cauretc/2021_KO_rnaseq/kallisto_dir --prefix_out
